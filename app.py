89906a6eed5bd79863bdb16107e873c4cb841b2c
Arifa Shaik <shaikarifa1830@gmail.com>
2025-08-06 22:01:42 +0530

    Fix model path and add .gitkeep to include model folder

--- app.py
+++ app.py
@@ -4,13 +4,30 @@ import joblib
 # Load the trained model
 model = joblib.load("model/emotion_model.pkl")
 
+# Map each emotion to an emoji
+emoji_map = {
+    'happy': '😊',
+    'angry': '😡',
+    'sad': '😢',
+    'love': '❤️',
+    'calm': '😌',
+    'fear': '😱',
+    'nervous': '😬',
+    'lonely': '🥺',
+    'surprised': '😲',
+    'disgusted': '🤢',
+    'embarrassed': '😳',
+    'confused': '🤔'
+}
+
 # Title
 st.title("🧠 MindPulse: Emotion Detector")
 
 # Text input
-user_input = st.text_input("Enter a sentence tstreamlit run app.pyo detect emotion:")
+user_input = st.text_input("Enter a sentence to detect emotion:")
 
 # Predict button
 if st.button("Predict Emotion"):
-        prediction = model.predict([user_input])[0]
-        st.success(f"Predicted Emotion: {prediction}")
+    prediction = model.predict([user_input])[0]
+    emoji = emoji_map.get(prediction, '')  # Get emoji or empty if not found
+    st.success(f"Predicted Emotion: {prediction} {emoji}")
